2229f3c7bfcf5a2bb628d6391310995798665cd4
Karn Kaul <karnkaul@gmail.com>
2023-07-16 20:53:43 -0700

    Add loger::log overload (calls info)

--- engine/include/logger/log.hpp
+++ engine/include/logger/log.hpp
@@ -10,9 +10,11 @@ void error(std::string_view category, std::string_view message, SrcLoc const& lo
 void warn(std::string_view category, std::string_view message, SrcLoc const& location = SrcLoc::current());
 void info(std::string_view category, std::string_view message, SrcLoc const& location = SrcLoc::current());
 void debug(std::string_view category, std::string_view message, SrcLoc const& location = SrcLoc::current());
+inline void log(std::string_view category, std::string_view message, SrcLoc const& location = SrcLoc::current()) { info(category, message, location); }
 
 inline void error(std::string_view message, SrcLoc const& location = SrcLoc::current()) { error("general", message, location); }
 inline void warn(std::string_view message, SrcLoc const& location = SrcLoc::current()) { warn("general", message, location); }
 inline void info(std::string_view message, SrcLoc const& location = SrcLoc::current()) { info("general", message, location); }
 inline void debug(std::string_view message, SrcLoc const& location = SrcLoc::current()) { debug("general", message, location); }
+inline void log(std::string_view message, SrcLoc const& location = SrcLoc::current()) { log("general", message, location); }
 } // namespace gen::logger
