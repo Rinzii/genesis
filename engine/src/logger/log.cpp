42b9a54d160b472773833160d94c2c4d2626f832
Ian Pike <Ianpike98@gmail.com>
2023-08-02 13:41:36 -0400

    Add the ability for logging to be disabled when GEN_NDEBUG is defined

--- engine/src/logger/log.cpp
+++ engine/src/logger/log.cpp
@@ -1,20 +1,32 @@
 #include "logger/log.hpp"
 #include "logger/instance.hpp"
 
-namespace gen {
-void logger::error(std::string_view category, std::string_view message, SrcLoc const& location) {
-	Instance::print(message, Context::make(category, Level::eError, location));
-}
+namespace gen
+{
+#ifndef GEN_NDEBUG
+    void logger::error(std::string_view category, std::string_view message, SrcLoc const& location)
+	{
+        Instance::print(message, Context::make(category, Level::eError, location));
+    }
 
-void logger::warn(std::string_view category, std::string_view message, SrcLoc const& location) {
-	Instance::print(message, Context::make(category, Level::eWarn, location));
-}
+    void logger::warn(std::string_view category, std::string_view message, SrcLoc const& location)
+	{
+        Instance::print(message, Context::make(category, Level::eWarn, location));
+    }
 
-void logger::info(std::string_view category, std::string_view message, SrcLoc const& location) {
-	Instance::print(message, Context::make(category, Level::eInfo, location));
-}
+    void logger::info(std::string_view category, std::string_view message, SrcLoc const& location)
+	{
+        Instance::print(message, Context::make(category, Level::eInfo, location));
+    }
 
-void logger::debug(std::string_view category, std::string_view message, SrcLoc const& location) {
-	Instance::print(message, Context::make(category, Level::eDebug, location));
-}
+    void logger::debug(std::string_view category, std::string_view message, SrcLoc const& location)
+	{
+        Instance::print(message, Context::make(category, Level::eDebug, location));
+    }
+#else // If GEN_NDEBUG is defined then diable logging. The creation of this symbol is created by cmake.
+    void logger::error(std::string_view category, std::string_view message, SrcLoc const& location) {}
+    void logger::warn(std::string_view category, std::string_view message, SrcLoc const& location) {}
+    void logger::info(std::string_view category, std::string_view message, SrcLoc const& location) {}
+    void logger::debug(std::string_view category, std::string_view message, SrcLoc const& location) {}
+#endif
 } // namespace gen
