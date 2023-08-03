23142cba498af0398ddf5cd9a03321bd12c916ef
Ian Pike <Ianpike98@gmail.com>
2023-08-03 10:53:04 -0400

    Disable logging if we are in release

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -5,7 +5,10 @@
 #include <logger/log.hpp>
 
 int main() {
+	// We don't want to initialize the logger in release mode.
+#ifndef GEN_NDEBUG
 	auto logger = gen::logger::Instance{}; // Required to initialize the logger
+#endif
 
 	gen::Application app;
 
