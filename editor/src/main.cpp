ed1f8651ea4789207b300d5e3573813a864b5283
Ian Pike <Ianpike98@gmail.com>
2023-08-04 11:16:14 -0400

    Allow logging in release builds

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -5,10 +5,7 @@
 #include <logger/log.hpp>
 
 int main() {
-	// We don't want to initialize the logger in release mode.
-#ifndef GEN_NDEBUG
 	auto logger = gen::logger::Instance{}; // Required to initialize the logger
-#endif
 
 	gen::Application app;
 
