998f912da3d82ec93b57c92286fbd8e8850e93e0
Kouros26 <kouroslf@gmail.com>
2023-08-04 19:33:05 -0400

    build error patch

--- editor/src/main.cpp
+++ editor/src/main.cpp
@@ -1,21 +1,16 @@
 // Copyright (c) 2023-present Genesis Engine contributors (see LICENSE.txt)
 
 #include <application.hpp>
-#include <logger/instance.hpp>
-#include <logger/log.hpp>
 
 int main() {
-	auto logger = gen::logger::Instance{};
 
 	gen::Application app;
-
-	gen::logger::info("Hello, world!");
-
+	
 	try {
 		app.run();
 	}
 	catch (std::exception const& e) {
-		gen::logger::error(e.what());
+		//add logger
 		return EXIT_FAILURE;
 	}
 
