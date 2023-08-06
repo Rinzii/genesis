c2a07cd08049cc8b5cb1cf099416eda332238b5c
Ian Pike <Ianpike98@gmail.com>
2023-08-06 02:43:43 -0400

    make windowing more safe

--- engine/include/windowing/window.hpp
+++ engine/include/windowing/window.hpp
@@ -4,6 +4,7 @@
 
 #include "core.hpp"
 
+#include <memory>
 #include <string>
 
 // TODO: Replace this with a proper implementation
@@ -13,46 +14,48 @@ struct GLFWwindow;
 
 // End Forward declarations
 
-namespace gen {
-
-class Window
+namespace gen
 {
-public:
-    Window(int width, int height, std::string title);
-    ~Window();
 
-	Window(const Window&) = delete;
-	Window& operator=(const Window&) = delete;
+	class Window
+	{
+	public:
+		Window(int width, int height, std::string title);
+		~Window();
 
-	// Member functions
+		Window(const Window &)			   = delete;
+		Window & operator=(const Window &) = delete;
+		Window(Window &&)				   = default;
+		Window & operator=(Window &&)	   = default;
 
-	GEN_NODISCARD bool shouldClose() const;
-	static void pollEvents() ;
+		// Member functions
 
+		GEN_NODISCARD bool shouldClose() const;
+		static void pollEvents();
 
+		// Getters
 
-	// Getters
+		GEN_NODISCARD int getWidth() const { return m_width; }
+		GEN_NODISCARD int getHeight() const { return m_height; }
+		GEN_NODISCARD const std::string & getTitle() const { return m_title; }
+		GEN_NODISCARD GLFWwindow * getHandle() const { return m_window.get(); }
 
-	GEN_NODISCARD int getWidth() const { return m_width; }
-	GEN_NODISCARD int getHeight() const { return m_height; }
-	GEN_NODISCARD const std::string& getTitle() const { return m_title; }
-	GEN_NODISCARD GLFWwindow* getHandle() const { return m_window; }
+		// TODO: Currently these functions will break the window. Have not yet implemented window resizing.
 
+		void SetWidth(int width) { m_width = width; }
+		void SetHeight(int height) { m_height = height; }
+		void SetTitle(const std::string & title) { m_title = title; }
 
-	// TODO: Currently these functions will break the window. Have not yet implemented window resizing.
+	private:
 
-	void SetWidth(int width) { m_width = width; }
-	void SetHeight(int height) { m_height = height; }
-	void SetTitle(const std::string& title) { m_title = title; }
+		// Callbacks
+		static void callback_error(int error, const char * description);
 
-private:
-	void init();
+		int m_width;
+		int m_height;
+		std::string m_title;
 
-	int m_width;
-	int m_height;
-	std::string m_title;
-
-	GLFWwindow* m_window;
-};
+		std::shared_ptr<GLFWwindow> m_window;
+	};
 
 } // namespace gen
--- engine/src/windowing/window.cpp
+++ engine/src/windowing/window.cpp
@@ -2,41 +2,74 @@
 
 #include "windowing/window.hpp"
 
+#include <format>
 #include "graphics/device.hpp"
 #include "logger/log.hpp"
 
 #include <GLFW/glfw3.h>
 
-
 #include <utility>
 
-namespace gen {
+namespace gen
+{
 
-    Window::Window(int width, int height, std::string title) : m_width{width}, m_height{height}, m_title{std::move(title)} { // NOLINT(cppcoreguidelines-pro-type-member-init)
-        init();
-    }
+	Window::Window(int width, int height, std::string title)
+		: m_width{width}, m_height{height}, m_title{std::move(title)} // NOLINT(cppcoreguidelines-pro-type-member-init)
+	{
+		if (!glfwInit()) // NOLINT(readability-implicit-bool-conversion)
+		{
+			gen::logger::error("windowing", "Failed to initialize GLFW");
+			throw std::runtime_error("Failed to initialize GLFW");
+		}
 
-	Window::~Window() {
-        glfwDestroyWindow(m_window);
-		glfwTerminate();
-    }
+		// Set window hints
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // TODO: Later add support for resizing of windows.
 
-	bool Window::shouldClose() const {
-        return glfwWindowShouldClose(m_window) != 0;
-    }
+		// Set callbacks
+		glfwSetErrorCallback(callback_error);
 
-	void Window::pollEvents() {
-        glfwPollEvents();
-    }
+		// Create window
+		GLFWwindow * window_ = glfwCreateWindow(m_width, m_height, m_title.c_str(), nullptr, nullptr);
+		m_window			 = std::shared_ptr<GLFWwindow>(window_,
+											   [](GLFWwindow * window)
+											   {
+												   glfwDestroyWindow(window);
+												   glfwTerminate();
+											   });
 
-	void Window::init() {
-        glfwInit();
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // TODO: Later add support for resizing of windows.
+		// Check if window was created
+		if (!m_window)
+		{
+			gen::logger::error("windowing", "Failed to create GLFW window");
+			throw std::runtime_error("Failed to create GLFW window");
+		}
 
-        m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), nullptr, nullptr);
+		// Report successful window creation
+		gen::logger::info("windowing", "Window instance constructed");
+	}
 
-    }
+	Window::~Window()
+	{
+		// This is here for simple information and may be removed later if we don't need to destruct anything later.
+		gen::logger::info("windowing", "Window instance destructed");
+	}
 
+	bool Window::shouldClose() const
+	{
+		return glfwWindowShouldClose(m_window.get()) != 0;
+	}
 
-} // namespace gwn
+	void Window::pollEvents()
+	{
+		glfwPollEvents();
+	}
+
+	// Callbacks
+
+	void Window::callback_error(int error, const char * description)
+	{
+		gen::logger::warn("windowing", std::format("GLFW Error: {} - {}", error, description));
+	}
+
+} // namespace gen
