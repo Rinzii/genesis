6928d10db719769c654248ebc87ae77e37df5cf0
Rinzii <ianpike98@gmail.com>
2023-08-27 15:51:30 -0400

    small adjustments

--- engine/include/gen/io/fileUtils.hpp
+++ engine/include/gen/io/fileUtils.hpp
@@ -6,5 +6,9 @@
 
 namespace gen::io
 {
+	/**
+	 * Get the absolute path of the current active executable.
+	 * @return Absolute path to the executable or on failure returns an empty path.
+	 */
 	std::filesystem::path getExecutablePath();
-}
+} // namespace gen::io
--- engine/src/io/fileUtils.cpp
+++ engine/src/io/fileUtils.cpp
@@ -3,14 +3,14 @@
 #include "gen/io/fileUtils.hpp"
 
 #include "gen/core/base/config/platform.hpp"
-#include <cstdint>
 
 #ifdef GEN_PLATFORM_WINDOWS
 	#include "gen/system/win32/windows.hpp"
 #elif defined(GEN_PLATFORM_LINUX) || defined(GEN_PLATFORM_APPLE)
 	#include <unistd.h>
 	#ifdef GEN_PLATFORM_APPLE
-		#include <mach-o/dyld.h> // Mac requires this
+		#include <mach-o/dyld.h> // required for _NSGetExecutablePath()
+		#include <cstdint>
 	#endif
 #endif
 
@@ -18,24 +18,24 @@ namespace gen::io
 {
 	static constexpr std::size_t maxPathSize_v{256};
 
-#ifdef GEN_PLATFORM_WINDOWS
 	std::filesystem::path getExecutablePath()
 	{
-		char buffer[maxPathSize_v];
-		DWORD length = GetModuleFileNameA(nullptr, buffer, maxPathSize_v);
+#ifdef GEN_PLATFORM_WINDOWS
+		wchar_t buffer[maxPathSize_v];
+		DWORD length = GetModuleFileNameW(nullptr, buffer, maxPathSize_v);
 
-		if (length == 0) { return std::filesystem::path(); }
+		if (length == 0) { return {}; }
 
 		return std::filesystem::path(buffer).parent_path();
-	}
+		// END GEN_PLATFORM_WINDOWS
+
 #elif defined(GEN_PLATFORM_LINUX) || defined(GEN_PLATFORM_APPLE)
-	std::filesystem::path getExecutablePath()
-	{
-		char buffer[maxPathSize_v];	// NOLINT
+
+		char buffer[maxPathSize_v]; // NOLINT
 
 		// Apple is slightly different from normal unix and requires a special function.
 	#ifdef GEN_PLATFORM_APPLE
-		uint32_t const outputtedLen = maxPathSize_v;
+		std::uint32_t const outputtedLen = maxPathSize_v;
 		if (_NSGetExecutablePath(buffer, &outputtedLen) == 0) // This fails if the buffer size is too small.
 		{
 			return std::filesystem::path(buffer).parent_path();
@@ -53,12 +53,13 @@ namespace gen::io
 
 		// Permission denied (We must be inetd with this app run as other than root).
 		return {};
-	}
+		// END defined(GEN_PLATFORM_LINUX) || defined(GEN_PLATFORM_APPLE)
+
 #else
-	std::filesystem::path getExecutablePath()
-	{
 		// If for some reason we are on an unsupported platform, just return an empty path.
 		return {};
-	}
+// END ELSE
 #endif
+	}
+
 } // namespace gen::io
