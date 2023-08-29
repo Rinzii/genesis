cd60358b7b36fb01b8b0c98bae6512e3db064f86
Rinzii <ianpike98@gmail.com>
2023-08-29 09:04:03 -0400

    ok getExecutablePath() should be pretty complete now

--- engine/src/io/fileUtils.cpp
+++ engine/src/io/fileUtils.cpp
@@ -2,7 +2,7 @@
 
 #include "gen/io/fileUtils.hpp"
 
-#include "gen/core/base/config/platform.hpp"
+#include "gen/core/base/config/platform.hpp" // GEN_PLATFORM_*
 
 
 #ifdef GEN_PLATFORM_WINDOWS
@@ -25,34 +25,10 @@ namespace gen::io
 }
 // END GEN_PLATFORM_WINDOWS
 
-#elif defined(GEN_PLATFORM_LINUX)
-
-#include <unistd.h>
-#include <linux/limits.h> // PATH_MAX
-
-namespace gen::io
-{
-	std::filesystem::path getExecutablePath()
-	{
-		char buffer[PATH_MAX]; // NOLINT
-		// Read the symbolic link pointing to the executable
-		ssize_t const outputtedLen = readlink("/proc/self/exe", buffer, PATH_MAX); // NOLINT
-		if (outputtedLen != -1)
-		{
-			// NOLINTNEXTLINE
-			buffer[outputtedLen] = '\0'; // Null-terminate the string
-			return std::filesystem::path(buffer).parent_path();
-		}
-
-		return {};
-	}
-}
-// END GEN_PLATFORM_LINUX
-
 #elif defined(GEN_PLATFORM_APPLE)
 
-	#include <mach-o/dyld.h> // required for _NSGetExecutablePath()
-	#include <cstdint>
+#include <mach-o/dyld.h> // required for _NSGetExecutablePath()
+#include <cstdint> // required for std::uint32_t
 
 namespace gen::io
 {
@@ -60,7 +36,7 @@ namespace gen::io
 	{
 		// On mac / ios paths can be no longer than 1024 characters.
 		char buffer[1024]; // NOLINT
-		std::uint32_t const outputtedLen = 1024;
+		std::uint32_t outputtedLen = sizeof(buffer);
 		if (_NSGetExecutablePath(buffer, &outputtedLen) == 0) // This fails if the buffer size is too small.
 		{
 			return std::filesystem::path(buffer).parent_path();
@@ -70,5 +46,15 @@ namespace gen::io
 	}
 }
 // END GEN_PLATFORM_APPLE
+
+#else // This should cover linux, unix, and android (does not cover bsd)
+
+namespace gen::io
+{
+	std::filesystem::path getExecutablePath()
+	{
+		return std::filesystem::read_symlink("/proc/self/exe").parent_path();
+	}
+}
 #endif
 
